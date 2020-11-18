79001a8527acccf24c51e4c38cb981ba8640f1be
xujiangjiang <xujiangjiang@njzhdun.com>
2020-11-18 16:23:47 +0800

    Initial commit

---
+++ consumer/src/main/java/com/gupao/FirstConsumer.java
@@ -0,0 +1,24 @@
+package com.gupao;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: qingshan
+ * @Date: 2018/10/20 17:04
+ * @Description: 咕泡学院，只为更好的你
+ */
+@Component
+@PropertySource("classpath:gupaomq.properties")
+@RabbitListener(queues = "${com.gupaoedu.firstqueue}", containerFactory="rabbitListenerContainerFactory")
+public class FirstConsumer {
+
+    @RabbitHandler
+    public void process(@Payload String str){
+        System.out.println("First Queue received msg : " + str);
+    }
+
+}
---
+++ consumer/src/main/java/com/gupao/RabbitConfig.java
@@ -0,0 +1,92 @@
+package com.gupao;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:gupaomq.properties")
+public class RabbitConfig {
+    @Value("${com.gupaoedu.firstqueue}")
+    private String firstQueue;
+
+    @Value("${com.gupaoedu.secondqueue}")
+    private String secondQueue;
+
+    @Value("${com.gupaoedu.thirdqueue}")
+    private String thirdQueue;
+
+    @Value("${com.gupaoedu.fourthqueue}")
+    private String fourthQueue;
+
+    @Value("${com.gupaoedu.directexchange}")
+    private String directExchange;
+
+    @Value("${com.gupaoedu.topicexchange}")
+    private String topicExchange;
+
+    @Value("${com.gupaoedu.fanoutexchange}")
+    private String fanoutExchange;
+
+    // 创建四个队列
+    @Bean("vipFirstQueue")
+    public Queue getFirstQueue(){
+        return new Queue(firstQueue);
+    }
+
+    @Bean("vipSecondQueue")
+    public Queue getSecondQueue(){
+        return new Queue(secondQueue);
+    }
+
+    @Bean("vipThirdQueue")
+    public  Queue getThirdQueue(){
+        return  new Queue(thirdQueue);
+    }
+
+    @Bean("vipFourthQueue")
+    public  Queue getFourthQueue(){
+        return  new Queue(fourthQueue);
+    }
+
+    // 创建三个交换机
+    @Bean("vipDirectExchange")
+    public DirectExchange getDirectExchange(){
+        return new DirectExchange(directExchange);
+    }
+
+    @Bean("vipTopicExchange")
+    public TopicExchange getTopicExchange(){
+        return new TopicExchange(topicExchange);
+    }
+
+    @Bean("vipFanoutExchange")
+    public FanoutExchange getFanoutExchange(){
+        return new FanoutExchange(fanoutExchange);
+    }
+
+    // 定义四个绑定关系
+    @Bean
+    public Binding bindFirst(@Qualifier("vipFirstQueue") Queue queue, @Qualifier("vipDirectExchange") DirectExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with("gupao.best");
+    }
+
+    @Bean
+    public Binding bindSecond(@Qualifier("vipSecondQueue") Queue queue, @Qualifier("vipTopicExchange") TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with("*.gupao.*");
+    }
+
+    @Bean
+    public Binding bindThird(@Qualifier("vipThirdQueue") Queue queue, @Qualifier("vipFanoutExchange") FanoutExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange);
+    }
+
+    @Bean
+    public Binding bindFourth(@Qualifier("vipFourthQueue") Queue queue, @Qualifier("vipFanoutExchange") FanoutExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange);
+    }
+
+}
